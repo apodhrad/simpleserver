d3eac06c5239ae65ccb76e491076f039420817f6
Andrej Podhradsky <apodhrad@redhat.com>
2023-05-05 21:45:18 +0200

    Refactor server
    Signed-off-by: Andrej Podhradsky <apodhrad@redhat.com>

--- server/simpleserver.go
+++ server/simpleserver.go
@@ -12,13 +12,80 @@ import (
 	"time"
 )
 
+const DEFAULT_PORT int = 8883
+
 const CTX_NAME_KEY string = "ctx_name"
 
 const CONTENT_TYPE_TEXT string = "text/plain"
 const CONTENT_TYPE_JSON string = "application/json"
 const CONTENT_TYPE_FILE string = "application/octet-stream"
 
-func SimpleHandler(dir string, path string) (string, func(w http.ResponseWriter, r *http.Request)) {
+type SimpleServer struct {
+	Port      int
+	Dir       string
+	server    *http.Server
+	ctx       context.Context
+	cancelCtx context.CancelFunc
+}
+
+func (s *SimpleServer) Start() (string, error) {
+	mux := http.NewServeMux()
+
+	err := filepath.Walk(s.Dir, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			mux.HandleFunc(simpleHandler(s.Dir, path))
+		}
+		return nil
+	})
+	if err != nil {
+		return "", err
+	}
+
+	s.ctx, s.cancelCtx = context.WithCancel(context.Background())
+	s.server = &http.Server{
+		Addr:    getAddr(s.Port),
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			// ctx = context.WithValue(ctx, CTX_NAME_KEY, ctxName)
+			return s.ctx
+		},
+	}
+	go func() {
+		fmt.Printf("Start server\n")
+		err := s.server.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("Server closed\n")
+		} else if err != nil {
+			fmt.Printf("error listening for server one: %s\n", err)
+		}
+		s.cancelCtx()
+	}()
+
+	time.Sleep(1 * time.Second)
+
+	return s.server.Addr, err
+}
+
+func (simpleServer *SimpleServer) Stop() error {
+	err := simpleServer.server.Shutdown(simpleServer.ctx)
+	if err != nil {
+		return err
+	}
+	<-simpleServer.ctx.Done()
+	return nil
+}
+
+func getAddr(port int) string {
+	if port == 0 {
+		port = DEFAULT_PORT
+	}
+	return fmt.Sprintf("localhost:%d", port)
+}
+
+func simpleHandler(dir string, path string) (string, func(w http.ResponseWriter, r *http.Request)) {
 	simpleHandler := func(w http.ResponseWriter, r *http.Request) {
 		fileBytes, err := os.ReadFile(path)
 		if err != nil {
@@ -30,56 +97,4 @@ func SimpleHandler(dir string, path string) (string, func(w http.ResponseWriter,
 	}
 	pattern := path[len(dir):]
 	return pattern, simpleHandler
-}
-
-type SimpleServer struct {
-	server    *http.Server
-	ctx       context.Context
-	cancelCtx context.CancelFunc
-}
-
-func (simpleServer *SimpleServer) Start(dir string) error {
-	mux := http.NewServeMux()
-
-	err := filepath.Walk(dir, func(path string, info fs.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if !info.IsDir() {
-			mux.HandleFunc(SimpleHandler(dir, path))
-		}
-		return nil
-	})
-	if err != nil {
-		return err
-	}
-
-	simpleServer.ctx, simpleServer.cancelCtx = context.WithCancel(context.Background())
-	simpleServer.server = &http.Server{
-		Addr:    ":8333",
-		Handler: mux,
-		BaseContext: func(l net.Listener) context.Context {
-			// ctx = context.WithValue(ctx, CTX_NAME_KEY, ctxName)
-			return simpleServer.ctx
-		},
-	}
-	go func() {
-		fmt.Printf("Start server\n")
-		err := simpleServer.server.ListenAndServe()
-		if errors.Is(err, http.ErrServerClosed) {
-			fmt.Printf("Server closed\n")
-		} else if err != nil {
-			fmt.Printf("error listening for server one: %s\n", err)
-		}
-		simpleServer.cancelCtx()
-	}()
-
-	time.Sleep(1 * time.Second)
-
-	return err
-}
-
-func (simpleServer *SimpleServer) Stop() {
-	simpleServer.server.Shutdown(simpleServer.ctx)
-	<-simpleServer.ctx.Done()
 }
--- server/simpleserver_test.go
+++ server/simpleserver_test.go
@@ -11,7 +11,8 @@ import (
 const STATUS_200_OK string = "200 OK"
 const STATUS_404_NOT_FOUND string = "404 Not Found"
 
-func assertRequest(t *testing.T, url string, expected string) {
+func assertRequest(t *testing.T, addr string, path string, expected string) {
+	url := "http://" + addr + path
 	resp, err := http.Get(url)
 	assert.Nil(t, err)
 	if resp != nil {
@@ -23,7 +24,8 @@ func assertRequest(t *testing.T, url string, expected string) {
 	assert.Equal(t, expected, string(actual))
 }
 
-func assertStatus(t *testing.T, url string, expectedStatus string) {
+func assertStatus(t *testing.T, addr string, path string, expectedStatus string) {
+	url := "http://" + addr + path
 	resp, err := http.Get(url)
 	if resp != nil {
 		defer resp.Body.Close()
@@ -33,7 +35,8 @@ func assertStatus(t *testing.T, url string, expectedStatus string) {
 	}
 }
 
-func assertRequestError(t *testing.T, url string) {
+func assertRequestError(t *testing.T, addr string, path string) {
+	url := "http://" + addr + path
 	resp, err := http.Get(url)
 	assert.NotNil(t, err)
 	if resp != nil {
@@ -41,27 +44,49 @@ func assertRequestError(t *testing.T, url string) {
 	}
 }
 func TestSimpleServer_00_empty(t *testing.T) {
-	server := SimpleServer{}
-	server.Start("test-resources/00_empty")
+	server := SimpleServer{Dir: "test-resources/00_empty"}
+	addr, err := server.Start()
 
-	assertStatus(t, "http://localhost:8333", STATUS_404_NOT_FOUND)
-	assertStatus(t, "http://localhost:8333/hello.txt", STATUS_404_NOT_FOUND)
-	assertStatus(t, "http://localhost:8333/hello/helloworld.txt", STATUS_404_NOT_FOUND)
+	assert.Nil(t, err)
+	assertStatus(t, addr, "", STATUS_404_NOT_FOUND)
+	assertStatus(t, addr, "/hello.txt", STATUS_404_NOT_FOUND)
+	assertStatus(t, addr, "/hello/helloworld.txt", STATUS_404_NOT_FOUND)
 
 	server.Stop()
-	assertRequestError(t, "http://localhost:8333")
+	assertRequestError(t, addr, "")
 }
 
 func TestSimpleServer_01_helloworld(t *testing.T) {
-	server := SimpleServer{}
-	server.Start("test-resources/01_helloworld")
+	server := SimpleServer{Dir: "test-resources/01_helloworld"}
+	addr, err := server.Start()
 
-	assertStatus(t, "http://localhost:8333", STATUS_404_NOT_FOUND)
-	assertStatus(t, "http://localhost:8333/hello.txt", STATUS_200_OK)
-	assertRequest(t, "http://localhost:8333/hello.txt", "Hello SimpleServer!")
-	assertStatus(t, "http://localhost:8333/hello/helloworld.txt", STATUS_200_OK)
-	assertRequest(t, "http://localhost:8333/hello/helloworld.txt", "Hello World!")
+	assert.Nil(t, err)
+	assertStatus(t, addr, "", STATUS_404_NOT_FOUND)
+	assertStatus(t, addr, "/hello.txt", STATUS_200_OK)
+	assertRequest(t, addr, "/hello.txt", "Hello SimpleServer!")
+	assertStatus(t, addr, "/hello/helloworld.txt", STATUS_200_OK)
+	assertRequest(t, addr, "/hello/helloworld.txt", "Hello World!")
 
 	server.Stop()
-	assertRequestError(t, "http://localhost:8333")
+	assertRequestError(t, addr, "")
+}
+
+func TestSimpleServer_02_multi(t *testing.T) {
+	server1 := SimpleServer{Port: 8881, Dir: "test-resources/02_multi/01_sub"}
+	addr1, err1 := server1.Start()
+
+	server2 := SimpleServer{Port: 8882, Dir: "test-resources/02_multi/02_sub"}
+	addr2, err2 := server2.Start()
+
+	assert.Nil(t, err1)
+	assert.Nil(t, err2)
+	assertRequest(t, addr1, "/hello.txt", "Hello from 01_sub")
+	assertRequest(t, addr2, "/hello.txt", "Hello from 02_sub")
+
+	server1.Stop()
+	assertRequestError(t, addr1, "")
+	// server2 should be still running
+	assertRequest(t, addr2, "/hello.txt", "Hello from 02_sub")
+	server2.Stop()
+	assertRequestError(t, addr2, "")
 }
