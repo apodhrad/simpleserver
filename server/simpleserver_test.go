a1cbd8b92f8585358cfa148a52ce6353152e1ec7
Andrej Podhradsky <apodhrad@redhat.com>
2023-05-05 20:58:25 +0200

    Extend tests
    Signed-off-by: Andrej Podhradsky <apodhrad@redhat.com>

--- server/simpleserver_test.go
+++ server/simpleserver_test.go
@@ -8,6 +8,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const STATUS_200_OK string = "200 OK"
+const STATUS_404_NOT_FOUND string = "404 Not Found"
+
 func assertRequest(t *testing.T, url string, expected string) {
 	resp, err := http.Get(url)
 	assert.Nil(t, err)
@@ -20,6 +23,16 @@ func assertRequest(t *testing.T, url string, expected string) {
 	assert.Equal(t, expected, string(actual))
 }
 
+func assertStatus(t *testing.T, url string, expectedStatus string) {
+	resp, err := http.Get(url)
+	if resp != nil {
+		defer resp.Body.Close()
+		assert.Equal(t, resp.Status, expectedStatus)
+	} else {
+		assert.Fail(t, err.Error())
+	}
+}
+
 func assertRequestError(t *testing.T, url string) {
 	resp, err := http.Get(url)
 	assert.NotNil(t, err)
@@ -27,12 +40,26 @@ func assertRequestError(t *testing.T, url string) {
 		defer resp.Body.Close()
 	}
 }
-
-func TestSimpleServer(t *testing.T) {
+func TestSimpleServer_00_empty(t *testing.T) {
 	server := SimpleServer{}
-	server.Start("test-resources/dir_01")
+	server.Start("test-resources/00_empty")
 
+	assertStatus(t, "http://localhost:8333", STATUS_404_NOT_FOUND)
+	assertStatus(t, "http://localhost:8333/hello.txt", STATUS_404_NOT_FOUND)
+	assertStatus(t, "http://localhost:8333/hello/helloworld.txt", STATUS_404_NOT_FOUND)
+
+	server.Stop()
+	assertRequestError(t, "http://localhost:8333")
+}
+
+func TestSimpleServer_01_helloworld(t *testing.T) {
+	server := SimpleServer{}
+	server.Start("test-resources/01_helloworld")
+
+	assertStatus(t, "http://localhost:8333", STATUS_404_NOT_FOUND)
+	assertStatus(t, "http://localhost:8333/hello.txt", STATUS_200_OK)
 	assertRequest(t, "http://localhost:8333/hello.txt", "Hello SimpleServer!")
+	assertStatus(t, "http://localhost:8333/hello/helloworld.txt", STATUS_200_OK)
 	assertRequest(t, "http://localhost:8333/hello/helloworld.txt", "Hello World!")
 
 	server.Stop()
